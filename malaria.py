5dc4e9c78e3f200d85e8425a24425615fc03ea93
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2020-05-26 13:39:37 +0530

    Update malaria.py

--- malaria.py
+++ malaria.py
@@ -15,8 +15,6 @@ model = Sequential()
 model.add(Convolution2D(filters=32,kernel_size=(3,3),  activation='relu',input_shape=(28, 28, 3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Convolution2D(filters=32,kernel_size=(3,3),activation='relu',))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 
